95f5eab26631abf58a65b14f740840771d8158e0
psvenz <svensson3000@gmail.com>
2014-06-13 23:49:10 +0200

    Removed debug code.

--- osmTileMachine/src/osmTileMachine/MainClass.java
+++ osmTileMachine/src/osmTileMachine/MainClass.java
@@ -218,7 +218,7 @@ public class MainClass {
 			ts.addSet(Geography.getTileSetForRegion(sessionConfiguration.getRequestedArea()));
 
 			System.out.println("Updating planet...");
-//			PlanetMaintainer.updatePlanet(sessionConfiguration);
+			PlanetMaintainer.updatePlanet(sessionConfiguration);
 
 
 

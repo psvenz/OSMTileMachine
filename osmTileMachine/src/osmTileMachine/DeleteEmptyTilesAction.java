87df9534d41531cb35caaf5c721c231a8b939994
psvenz <svensson3000@gmail.com>
2014-10-29 21:09:58 +0100

    implemented caching of file sizes
    performace gains

--- osmTileMachine/src/osmTileMachine/DeleteEmptyTilesAction.java
+++ osmTileMachine/src/osmTileMachine/DeleteEmptyTilesAction.java
@@ -1,6 +1,7 @@
 package osmTileMachine;
 
 import java.io.File;
+import java.util.Hashtable;
 
 public class DeleteEmptyTilesAction extends Action{
 
@@ -16,6 +17,7 @@ public class DeleteEmptyTilesAction extends Action{
 
 	@Override
 	void runAction(Configuration sessionConfiguration) throws Exception {
+		diskFileSizes = new Hashtable<String, Long>();
 		TileSet ts = tile.getAllHigherZoomTiles(deleteZoomLevel);
 		ts.tileSetIteratorStart();
 		long totalFilesExamined = 0;
@@ -26,38 +28,61 @@ public class DeleteEmptyTilesAction extends Action{
 
 			int windowSize = 3;
 			long maxFileSizeNearby = 0;
-			
-			
+
+
 			for (int xDelta = -windowSize;xDelta <= windowSize; xDelta++)
 			{
 				for (int yDelta = -windowSize;yDelta <= windowSize; yDelta++)
 				{
 					String fileName = RenderAction.getImageFileName(new Tile(t.getX()+xDelta,t.getY()+yDelta,t.getZ(),""), sessionConfiguration);
-					File dFile = new File(fileName);
-					long fileSize = dFile.length();
+
+
+					Long fileSize = lookupFileSize(fileName);
+
 					if (fileSize > maxFileSizeNearby) maxFileSizeNearby = fileSize;
 				}
 			}
-			
+
 			String fileName = RenderAction.getImageFileName(t, sessionConfiguration);
 			File dFile = new File(fileName);
-			
-			
+
+
 			if (maxFileSizeNearby < minFileSize){
-//				MessagePrinter.debug(sessionConfiguration, "deleting file " + fileName + " filesizeNearby=" + maxFileSizeNearby);
+				//				MessagePrinter.debug(sessionConfiguration, "deleting file " + fileName + " filesizeNearby=" + maxFileSizeNearby);
 				dFile.delete();
 				deletedFiles++;
 			}
 			else
 			{
-//				MessagePrinter.debug(sessionConfiguration, "keeping  file " + fileName + " filesizeNearby=" + maxFileSizeNearby);
+				//				MessagePrinter.debug(sessionConfiguration, "keeping  file " + fileName + " filesizeNearby=" + maxFileSizeNearby);
 			}
 		}
 		MessagePrinter.notify(sessionConfiguration, "DeleteEmptyTiles: deleted " + deletedFiles + " of " + totalFilesExamined + " files");
 
 	}
 
+	Hashtable<String, Long> diskFileSizes;
 
+	private long lookupFileSize(String fileName) {
+		// TODO Auto-generated method stub
+
+		// Cached filesize?		
+		Long n = diskFileSizes.get(fileName);
+		if (n != null) {
+			return n;
+		}
+
+
+		File dFile = new File(fileName);
+		long fileSize = dFile.length();
+		diskFileSizes.put(fileName, fileSize);
+
+
+
+		return fileSize;
+
+
+	}
 
 	@Override
 	String getActionInHumanReadableFormat() {

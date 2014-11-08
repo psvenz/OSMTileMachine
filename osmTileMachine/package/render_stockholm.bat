b72cb93f2bda8348c05288ad1aea91e97a2bc44b
psvenz <svensson3000@gmail.com>
2014-11-09 00:23:15 +0100

    Example changed to Stockholm z18

---
+++ osmTileMachine/package/render_stockholm.bat
@@ -0,0 +1,3 @@
+java -jar osmtilemachine.jar -source downloadsweden -renderarea box=17.98,59.27,18.18,59.38 -rules default.mrules -verbose -maxzoom 18 -generatelowzoom
+start webroot\index.html
+pause

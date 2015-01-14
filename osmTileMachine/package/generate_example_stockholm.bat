7a2fee58175888e7e7bc7f5e23fc7d0569a7c81b
psvenz <svensson3000@gmail.com>
2015-01-14 22:32:29 +0100

    added lazyupdate example

--- osmTileMachine/package/generate_example_stockholm.bat
+++ osmTileMachine/package/generate_example_stockholm.bat
@@ -1,3 +1,3 @@
-java -jar osmtilemachine.jar -source downloadsweden -renderarea box=17.6;59.1;18.6;59.6 -rules default.mrules -verbose -maxzoom 17 -generatelowzoom
+java -jar osmtilemachine.jar -source downloadsweden -renderarea box=17.6;59.1;18.6;59.6 -maxzoom 15
 start webroot\example_leaflet_Stockholm.html
 pause

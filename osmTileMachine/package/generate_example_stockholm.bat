597f0cf0c5f71d6b0b861442f654be3069ed2d52
psvenz <svensson3000@gmail.com>
2015-10-02 14:39:35 +0200

    removed browser autostart

--- osmTileMachine/package/generate_example_stockholm.bat
+++ osmTileMachine/package/generate_example_stockholm.bat
@@ -1,3 +1,2 @@
-java -jar osmtilemachine.jar -source downloadsweden -renderarea box=17.6;59.1;18.6;59.6 -maxzoom 15
-start webroot\example_leaflet_Stockholm.html
+java -jar osmtilemachine.jar -source downloadsweden -rules mtbmap-nextgeneration.mrules -renderarea file=mtbscalesweden.osm -maxzoom 13 -firstaction 140 -verbose
 pause
---
+++ osmTileMachine/package/generate_example_vänern.bat
@@ -0,0 +1,2 @@
+java -jar osmtilemachine.jar -source downloadsweden -renderarea vanernvatternmalarentest -rules mtbmap-nextgeneration.mrules -maxzoom 17
+pause
